80fadba456072bff14b3172e3d9341514cb20348
KarolNiewiadomski <karolniewiadomski@outlook.com>
2025-01-12 13:13:56 +0100

    created a stantardised product card for future products

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import HomeCards from "./components/HomeCards";
+import Products from "./components/Products";
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <HomeCards />
+      <Products />
     </>
   );
 };
