0cc49289e0222304454d2b91546e92ac74a4c742
KarolNiewiadomski <karolniewiadomski@outlook.com>
2025-01-14 16:45:56 +0100

    added a function that will return a componen on a 404 page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,7 +2,9 @@ import { Link } from "react-router-dom";
 import { FaExclamationTriangle } from "react-icons/fa";
 
 const NotFound = () => {
-  return <div></div>;
+  return (
+    <section className="text-center flex flex-col justify-center"></section>
+  );
 };
 
 export default NotFound;
