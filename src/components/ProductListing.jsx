1c26e64b76a5efb223207f17132475afaac6d0cc
KarolNiewiadomski <karolniewiadomski@outlook.com>
2025-01-17 18:13:26 +0100

    seperated productcards into a seperate component

---
+++ src/components/ProductListing.jsx
@@ -0,0 +1,34 @@
+import { FaStar } from "react-icons/fa";
+
+const ProductListing = ({ movie }) => {
+  return (
+    <div className="group relative bg-gray-800 rounded-lg shadow-lg overflow-hidden">
+      <div className="relative">
+        <img
+          alt={movie.title || movie.name}
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          className="aspect-square w-full object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"
+        />
+        <div className="absolute inset-0 bg-black bg-opacity-60 opacity-0 group-hover:opacity-100 transition-opacity flex items-center justify-center p-4">
+          <p className="text-sm text-white">{movie.overview}</p>
+        </div>
+      </div>
+      <div className="p-4">
+        <h3 className="text-lg font-bold text-white">
+          {movie.title || movie.name}
+        </h3>
+        <div className="flex items-center justify-between mt-2">
+          <span className="text-sm font-medium text-yellow-400">
+            <FaStar className="inline text-lg mb-1 mr-2" />
+            {movie.vote_average.toFixed(1)}
+          </span>
+          <button className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400">
+            Add to WatchList
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductListing;
--- src/components/ProductListings.jsx
+++ src/components/ProductListings.jsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from "react";
-import { FaStar } from "react-icons/fa";
+import React, { useState, useEffect } from "react";
+import ProductListing from "./ProductListing";
 import LoadMore from "./LoadMore";
 
 const ProductListings = () => {
@@ -33,45 +33,15 @@ const ProductListings = () => {
     <div className="bg-white">
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <h2 className="text-2xl font-bold tracking-tight text-gray-900">
-          Popular Movies & TV Shows
+          Currently Trending
         </h2>
 
-        <div>
-          <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-            {visibleMovies.map((movie) => (
-              <div
-                key={movie.id}
-                className="group relative bg-gray-800 rounded-lg shadow-lg overflow-hidden"
-              >
-                <div className="relative">
-                  <img
-                    alt={movie.title || movie.name}
-                    src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                    className="aspect-square w-full object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"
-                  />
-                  <div className="absolute inset-0 bg-black bg-opacity-60 opacity-0 group-hover:opacity-100 transition-opacity flex items-center justify-center p-4">
-                    <p className="text-sm text-white">{movie.overview}</p>
-                  </div>
-                </div>
-                <div className="p-4">
-                  <h3 className="text-lg font-bold text-white">
-                    {movie.title || movie.name}
-                  </h3>
-                  <div className="flex items-center justify-between mt-2">
-                    <span className="text-sm font-medium text-yellow-400">
-                      <FaStar className="inline text-lg mb-1 mr-2" />
-                      {movie.vote_average.toFixed(1)}
-                    </span>
-                    <button className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400">
-                      Add to WatchList
-                    </button>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-          {visibleCount < movies.length && <LoadMore handleClick={loadMore} />}
+        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          {visibleMovies.map((movie) => (
+            <ProductListing key={movie.id} movie={movie} />
+          ))}
         </div>
+        {visibleCount < movies.length && <LoadMore handleClick={loadMore} />}
       </div>
     </div>
   );
